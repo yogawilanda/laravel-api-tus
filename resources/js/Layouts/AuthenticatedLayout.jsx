f9a38649b52843da13b2f141b3d4c7facbef8c88
Yoga Wilanda <hyoga.wilanda@gmail.com>
2024-03-06 12:27:19 +0700

    Add empty NavLink in AuthenticatedLayout.jsx

--- resources/js/Layouts/AuthenticatedLayout.jsx
+++ resources/js/Layouts/AuthenticatedLayout.jsx
@@ -24,6 +24,10 @@ export default function Authenticated({ user, header, children }) {
                                 <NavLink href={route('dashboard')} active={route().current('dashboard')}>
                                     Dashboard
                                 </NavLink>
+                                
+                                <NavLink href={route('dashboard')} active={route().current('dashboard')}>
+                                    
+                                </NavLink>
                             </div>
                         </div>
 
